b429d5c4a6a4ff1b72c8b160a3473bbbe05e9da4
Ahmad <sohail.yu@gmail.com>
2020-12-07 19:12:45 -0700

    adding user date file

--- ansible/project/files/user_data.sh
+++ ansible/project/files/user_data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo yum install git -y
 sudo amazon-linux-extras install ansible -y
-sudo git clone https://github.com/sohailyu/DevOps-References.git ./tmp/ 
-cd /tmp sudo && ansible-playbook ansible/project/s3.yml
+sudo git clone https://github.com/sohailyu/DevOps-References.git /tmp/abcd 
+cd /tmp/abcd  ansible-playbook ansible/project/s3.yml
