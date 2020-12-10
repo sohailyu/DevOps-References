73f9d34f1636e659d93d5fbed80dfccb25d577fe
Ahmad <sohail.yu@gmail.com>
2020-12-10 12:13:54 -0700

    adding user.data

---
+++ ansible/roles/alarm/files/user_data.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo yum install git -y
+sudo amazon-linux-extras install ansible -y
+sudo git clone https://github.com/sohailyu/DevOps-References.git /tmp/abcd 
+cd /tmp/abcd  ansible-playbook ansible/playbook/alarm.yml
