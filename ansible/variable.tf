0db19423a23b2a94efa033e97f9c486589aca0c0
Ahmad Yusufzai <42722445+sohailyu@users.noreply.github.com>
2020-12-15 17:37:28 -0700

    Create variable.tf

---
+++ ansible/variable.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "assign_public_ip" {
+  default = "true"
+}
+
+variable "owners" {
+  default = "778455851442"
+}
+# variable "ssh-key" {
+# }
+variable "dev_key_name" {
+}
+
+variable "jenkins_key" {
+}
