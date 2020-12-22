b3e3fa9d68051a91ab2f5a4708768ed6438476c9
Ahmad Yusufzai <42722445+sohailyu@users.noreply.github.com>
2020-12-22 11:49:06 -0700

    Update user_data.sh

--- ansible/roles/alarm/files/user_data.sh
+++ ansible/roles/alarm/files/user_data.sh
@@ -2,4 +2,4 @@
 sudo yum install git -y
 sudo amazon-linux-extras install ansible -y
 sudo git clone https://github.com/sohailyu/DevOps-References.git /tmp/abcd 
-cd /tmp/abcd  ansible-playbook ansible/playbook/alarm.yml
+cd /tmp/abcd && ansible-playbook ansible/playbooks/02_InstallingSoftware.yml
